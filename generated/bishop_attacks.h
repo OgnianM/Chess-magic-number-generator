7e104162b49eb52b42833af4597c2902b6f111ed
Ognyan Mirev <12432824+OgnianM@users.noreply.github.com>
2021-12-14 13:53:24 +0000

    Add files via upload

---
+++ generated/bishop_attacks.h
@@ -0,0 +1,5 @@
+#ifndef bishop_ATTACKS_HEADER
+#define bishop_ATTACKS_HEADER
+
+uint64_t get_bishop_attacks(uint64_t position, int piece_idx);
+#endif
---
+++ generated/rook_attacks.h
@@ -0,0 +1,5 @@
+#ifndef rook_ATTACKS_HEADER
+#define rook_ATTACKS_HEADER
+
+uint64_t get_rook_attacks(uint64_t position, int piece_idx);
+#endif
